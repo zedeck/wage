eaffae1d20de5f12b1663f0dd435b37d63339e8a
Godwill Makhubela <godwill@mineware.co.za>
2023-07-10 09:33:19 +0200

    updated git ignore

---
+++ server/middleware/auth.js
@@ -0,0 +1,21 @@
+const jwt = require("jsonwebtoken");
+
+const config = process.env;
+
+const verifyToken = (req, res, next) => {
+  const token =
+    req.body.token || req.query.token || req.headers["x-access-token"];
+
+  if (!token) {
+    return res.status(403).send("SignIn or SignUp for Access");
+  }
+  try {
+    const decoded = jwt.verify(token, config.TOKEN_KEY);
+    req.user = decoded;
+  } catch (err) {
+    return res.status(401).send("Session Expired");
+  }
+  return next();
+};
+
+module.exports = verifyToken;
